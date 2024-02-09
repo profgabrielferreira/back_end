a5e655010aa90eb3341301d855659d5808d8ce18
profgabrielferreira <134711762+profgabrielferreira@users.noreply.github.com>
2024-02-09 11:51:06 -0300

    Add files via upload

---
+++ gabriel_2024_1.js
@@ -0,0 +1,25 @@
+exercicio
+1) crie uma função que some 4 numeros
+
+function Somar(a,b,c,d){
+    let soma = a + b + c + d;
+    return soma;
+}
+
+2) crie uma função que calcule a sua idade
+function CalculaIdade(anoAtual, anoNascimento){
+    let idade = anoAtual - anoNascimento;
+    return idade;
+}
+
+3) crie uma função que some tres notas e calcule a media
+function CalculaMedia(av1, av2, av3){
+    let somar = (av1 + av2 + av3);
+    let media = somar/3;
+    return media; 
+}
+function CalculaMedia(av1, av2, av3){
+    let media= (av1 + av2 + av3)/3;
+    return media;
+}
+
